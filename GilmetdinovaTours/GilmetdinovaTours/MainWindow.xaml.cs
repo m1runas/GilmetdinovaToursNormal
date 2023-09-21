3e579481d7fbf1c3f7e3df9a39faab09f532635a
m1runas <gilmetdinovaruslana@gmail.com>
2023-09-21 17:35:45 +0500

    Каркас приложения

---
+++ GilmetdinovaTours/GilmetdinovaTours/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GilmetdinovaTours
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelPage());
+            manager.MainFrame = MainFrame;
+        }
+
+        private void bntback_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainFrame_ContentRendered_1(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                btnback.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnback.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
